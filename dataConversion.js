0c81de40beb0b845ccc7e4a9253d6afe2d846f14
SChavan77 <142131478+SChavan77@users.noreply.github.com>
2024-01-16 10:58:16 +0000

    Conversion of Data types

--- dataConversion.js
+++ dataConversion.js
@@ -16,10 +16,35 @@ console.log(num)
 /**
  * null -> false
  * "" ->false
- * ""
+ * 0 ->false
+ * 1-> true
+ * "any value" ->true
+ * undefined ->false
+ */
+let booleanNum=1;
+let booleanValue=Boolean(booleanNum)
+console.log(booleanNum)
+console.log(booleanValue)
+
+/**
+ * 33->"33"
+ * null (it will display a bit bold) -> null
+ * undefined->undefined
  * 
  */
-let booleanNum=null;
-let booleanValue=Boolean(booleanNum)
-comsole.log(booleanNum)
-console.log(booleanValue)
+
+let stringNum=undefined;
+let stringValue=String(stringNum)
+console.log(typeof stringNum)
+console.log(stringNum)
+console.log(typeof stringValue)
+console.log(stringValue)
+
+
+
+
+
+
+
+
+
