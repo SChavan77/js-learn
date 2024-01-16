9de02d7669a0ad5d10807a699c47e268150ca470
SChavan77 <142131478+SChavan77@users.noreply.github.com>
2024-01-16 10:30:09 +0000

    Type Conversion

---
+++ dataConversion.js
@@ -0,0 +1,25 @@
+console.log("Hi I'm Sushmitha");
+let score=true
+let num=Number(score)
+console.log(typeof score)
+console.log(typeof num)
+/*
+"33" ->33
+"33abc"->NaN
+null ->0
+true->1
+undefined ->NaN
+*/
+console.log(score)
+console.log(num)
+
+/**
+ * null -> false
+ * "" ->false
+ * ""
+ * 
+ */
+let booleanNum=null;
+let booleanValue=Boolean(booleanNum)
+comsole.log(booleanNum)
+console.log(booleanValue)
